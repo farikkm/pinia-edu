68d3de86196b94bef8030aa68342f6f151617fda
farikkm <140306287+farikkm@users.noreply.github.com>
2024-03-24 16:28:09 +0500

    Delete .output directory

--- .output/public/_nuxt/Do49bSdM.js
+++
@@ -1,1 +0,0 @@
-import{y as e,q as r}from"./Sngmmtsx.js";const d=e((t,a)=>r("/search"));export{d as default};
--- .output/server/chunks/build/Header-styles.BJ-0crI3.mjs
+++
@@ -1,6 +0,0 @@
-const Header_vue_vue_type_style_index_0_scoped_f6e6c844_lang = ".title[data-v-f6e6c844]{font-family:Comic Neue}";
-
-const HeaderStyles_BJ0crI3 = [Header_vue_vue_type_style_index_0_scoped_f6e6c844_lang];
-
-export { HeaderStyles_BJ0crI3 as default };
-//# sourceMappingURL=Header-styles.BJ-0crI3.mjs.map
